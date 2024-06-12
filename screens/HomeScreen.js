75d847f6ef46f652a4f35ed7dd049c284d584944
Julliet Nyaware <julienyaware@gmail.com>
2024-06-12 12:27:46 -0500

    Added background image to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, ImageBackground, StyleSheet  } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+
+const backgroundImage = require('../assets/backgroundImage.jpg');
 
 export default function HomeScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
+        <View style={styles.container}>
+        <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.overlay}>
+          <Text style={styles.text}>This is your content</Text>
+          <Button
+          tyle={styles.text}
           onPress={() => navigation.navigate('Notifications')}
           title="Go to notifications"
         />
+        </View>
+        </ImageBackground>
+
       </View>
       );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
